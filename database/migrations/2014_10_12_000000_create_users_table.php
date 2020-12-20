fda2a7c1974dcf14ee0713ee63b45e885a607e6e
Divaltor <kuklavudu78@gmail.com>
2020-12-20 16:29:55 +0200

    Updated migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,11 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->increments('id');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('username')->nullable();
+            $table->string('telegram_id', 16)->unique();
             $table->rememberToken();
             $table->timestamps();
         });
